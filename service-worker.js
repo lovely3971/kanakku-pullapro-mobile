903628561d9914fdec090c749ef10e1cc2281650
lovely3971 <lovelyshoppekovai@gmail.com>
2026-08-28 12:35:05 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kanakku-pulla-pro-v1';
+const CACHE='kanakku-pulla-pro-v20260828-desktop-keyboard-tracking';
 const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
