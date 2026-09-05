23f14bd5b3c66c95b821c17218b8ecaced4a614a
lovely3971 <lovelyshoppekovai@gmail.com>
2026-09-05 22:05:01 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kanakku-pulla-pro-v8-print-flow-ios-fix';
+const CACHE='kanakku-pulla-pro-v7-mobile-print-preview-flow';
 const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./activate-design.webp','./shop-design.webp','./refresh-buffer-logo.jpeg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
