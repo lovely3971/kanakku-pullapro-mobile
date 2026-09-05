5ef540c91eb4c2825be53d7779f44eae66a8681e
lovely3971 <lovelyshoppekovai@gmail.com>
2026-09-05 20:39:28 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kanakku-pulla-pro-v3-optimized-assets';
+const CACHE='kanakku-pulla-pro-v5-mobile-bluetooth-58-80';
 const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./activate-design.webp','./shop-design.webp','./refresh-buffer-logo.jpeg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
